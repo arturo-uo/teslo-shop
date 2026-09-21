8bf120a3b71875e495b7f2d80620f6c9631c8fad
Arturo Urosa <arturo_uo@hotmail.com>
2026-09-21 15:34:06 -0600

    File namer

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,8 @@
 import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
 import { FilesService } from './files.service'
-import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from './helpers/fileFilter.helper';
+import { FileInterceptor } from '@nestjs/platform-express'
+import { fileFilter, fileNamer } from './helpers'
+import { diskStorage } from 'multer'
 
 @Controller('files')
 export class FilesController {
@@ -9,7 +10,12 @@ export class FilesController {
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
-    fileFilter: fileFilter
+    fileFilter: fileFilter,
+    limits:{fileSize: 100000},
+    storage: diskStorage({
+      destination:'./static/uploads',
+      filename: fileNamer
+    })
   }))
   uploadProductImage(@UploadedFile() file: Express.Multer.File)
   {
