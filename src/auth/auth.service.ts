1e4bfff12fbe248017e6a980e5d35bd61408a9ba
Arturo Urosa <arturo_uo@hotmail.com>
2026-09-22 16:20:25 -0600

    Registro y login de usuarios

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto, LoginUserDto } from './dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository, DataSource } from 'typeorm'
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>) { }
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto
+      const user = this.userRepository.create({
+        ...userData, 
+        password: bcrypt.hashSync(password, 10)
+      })
+      await this.userRepository.save(user)
+      //delete user['password']
+      //return user
+
+      const { password: _, ...userWithoutPassword } = user
+      return userWithoutPassword
+    }
+    catch (error: Error | any) {
+      this.handleDBExceptions(error)
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto
+    const user = await this.userRepository.findOne({
+      where: {email},
+      select: {email:true, password:true}
+    })
+    if(!user)
+      throw new UnauthorizedException('No valid email')
+
+    if(!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('No valid password')
+
+    return user
+    // try {
+      
+    // }
+    // catch (error: Error | any) {
+    //   this.handleDBExceptions(error)
+    // }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  private handleDBExceptions(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail)
+    }
+    this.logger.log(error)
+    throw new InternalServerErrorException('Unexpected error, check server logs');
   }
 }
